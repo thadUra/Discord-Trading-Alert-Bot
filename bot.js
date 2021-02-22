a99b7043d2a480695cbe1fea7ff727f4eb2b11bf
thaddziura <49488898+thaddziura@users.noreply.github.com>
2021-02-21 23:36:17 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,4 @@ client.on('message', message => {
     }
 });
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
